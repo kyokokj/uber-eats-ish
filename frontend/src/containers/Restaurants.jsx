c5c05ed9f34a5fc8756598760a2824451f515048
coco <coco@example.com>
2021-07-29 16:25:16 +1200

    Displayed data on Restaurants page

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,8 +1,15 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useReducer, useEffect } from "react";
 import styled from "styled-components";
 
 // APIs
 import { fetchRestaurants } from "../apis/restaurants";
+
+// reducers
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer,
+} from "../reducers/restaurants";
 
 // images
 import MainLogo from "../images/logo.png";
@@ -27,8 +34,18 @@ const MainCover = styled.img`
 `;
 
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
   useEffect(() => {
-    fetchRestaurants().then((data) => console.log(data));
+    dispatch({ type: restaurantsActionTypes.FETCHING });
+    fetchRestaurants().then((data) =>
+      dispatch({
+        type: restaurantsActionTypes.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants,
+        },
+      })
+    );
   }, []);
 
   return (
@@ -39,6 +56,9 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {state.restaurantsList.map((restaurant) => (
+        <div>{restaurant.name}</div>
+      ))}
     </Fragment>
   );
 };
